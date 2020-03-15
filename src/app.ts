177d1a00b4469876d045f91b46db1404a2deec8f
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2020-03-15 17:45:50 +0300

    app.ts updated according to eslint

--- src/app.ts
+++ src/app.ts
@@ -1,40 +1,40 @@
-import "reflect-metadata";
-import config from "./config";
-import express, { Request, Response, NextFunction } from "express";
-import { connectDatabase } from "./database";
-import bodyParser from "body-parser";
-import routes from "./api";
+import 'reflect-metadata';
+import express, { Request, Response, NextFunction } from 'express';
+import bodyParser from 'body-parser';
+import config from './config';
+import { connectDatabase } from './database';
+import routes from './api';
 
-//Create app instance
+// Create app instance
 const app = express();
 
-//Middleware
+// Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//Catch 400
+// Catch 400
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-    console.log(err.stack);
-    res.status(400).send(`Url not found error: ${err}`);
-    next();
+  console.log(err.stack);
+  res.status(400).send(`Url not found error: ${err}`);
+  next();
 });
 
-//Catch 500
+// Catch 500
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-    console.log(err.stack);
-    res.status(500).send(`Bad request error: ${err}`);
-    next();
+  console.log(err.stack);
+  res.status(500).send(`Bad request error: ${err}`);
+  next();
 });
 
 // Load API routes
 app.use(config.api.prefix, routes());
 
-const port = config.port;
+const { port } = config;
 
 connectDatabase();
 
 app.listen(port, () => {
-    console.log(`🛡️  Server listening on port: ${config.port} 🛡️`);
+  console.log(`🛡️  Server listening on port: ${config.port} 🛡️`);
 });
 
 export default app;
